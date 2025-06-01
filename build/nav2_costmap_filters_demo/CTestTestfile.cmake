272024d3f890647bb81f915c94cc2773d9029c57
Yankos <xardziopl@gmail.com>
2025-06-01 16:35:10 +0200

    ---

---
+++ build/nav2_costmap_filters_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/szymon/Documents/MIAPR/Project_NAV2_przyklad/navigation2_tutorials/nav2_costmap_filters_demo
+# Build directory: /home/szymon/Documents/MIAPR/Project_NAV2_przyklad/build/nav2_costmap_filters_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/nav2_gradient_costmap_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/szymon/Documents/MIAPR/Project_NAV2_przyklad/navigation2_tutorials/nav2_gradient_costmap_plugin
+# Build directory: /home/szymon/Documents/MIAPR/Project_NAV2_przyklad/build/nav2_gradient_costmap_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
