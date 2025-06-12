75f4968ae117abdf90a0f9480c8f45f645f86b46
Yankos-G <xardziopl@gmail.com>
2025-06-12 14:39:45 +0000

    ---

---
+++ build/nav2_RRTstar_planner/CMakeFiles/nav2_RRTstar_planner_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nav2_RRTstar_planner_plugin.dir/src/RRT_star_planner.cpp.o"
+  "CMakeFiles/nav2_RRTstar_planner_plugin.dir/src/RRT_star_planner.cpp.o.d"
+  "libnav2_RRTstar_planner_plugin.pdb"
+  "libnav2_RRTstar_planner_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nav2_RRTstar_planner_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
